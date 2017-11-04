0fff6a82a8d26e2f600b33277fd99d7201e16326
abiduzz420 <abiduzair420@gmail.com>
2017-11-05 02:43:48 +0530

    bob completed

--- bob/src/bob.clj
+++ bob/src/bob.clj
@@ -2,5 +2,9 @@
    (:require [clojure.string :as str]))
 
 (defn response-for [tell-me]
-      (str/ends-with? (str tell-me) "?")) 
+      (cond
+        (str/blank? tell-me) "Fine. Be that way!"
+        (and (= tell-me (str/upper-case tell-me)) (not= (str/upper-case tell-me) (str/lower-case tell-me))) "Whoa, chill out!"
+        (str/ends-with? (str tell-me) "?") "Sure."
+        :else "Whatever.")) 
 
